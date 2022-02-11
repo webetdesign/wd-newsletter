978d0f8893550c707248a64ea16eda49d900646f
ROBERT Benjamin <emailpourfb90@gmail.com>
2022-02-11 10:49:33 +0100

    text newsletter log

--- src/Http/TransparentPixelResponse.php
+++ src/Http/TransparentPixelResponse.php
@@ -6,30 +6,24 @@ use Symfony\Component\HttpFoundation\Response;
 
 class TransparentPixelResponse extends Response
 {
-    /**
-     * Base 64 encoded contents for 1px transparent gif and png
-     * @var string
-     */
-    const IMAGE_CONTENT =
-        'R0lGODlhAQABAJAAAP8AAAAAACH5BAUQAAAALAAAAAABAAEAAAICBAEAOw=='
-    ;
+
 
     /**
      * The response content type
      * @var string
      */
-    const CONTENT_TYPE = 'image/gif';
+    const CONTENT_TYPE = 'image/png';
 
     /**
      * Constructor
      */
     public function __construct()
     {
-        $content = base64_decode(self::IMAGE_CONTENT);
-        parent::__construct($content);
-        $this->headers->set('Content-Type', self::CONTENT_TYPE);
-        $this->setPrivate();
-        $this->headers->addCacheControlDirective('no-cache', true);
-        $this->headers->addCacheControlDirective('must-revalidate', true);
+        $pixel = sprintf('%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c', 71, 73, 70, 56, 57, 97, 1, 0, 1, 0, 128, 255, 0, 192, 192, 192, 0, 0, 0, 33, 249, 4, 1, 0, 0, 0, 0, 44, 0, 0, 0, 0, 1, 0, 1, 0, 0, 2, 2, 68, 1, 0, 59);
+        parent::__construct($pixel);
+        $this->headers->set('Content-type', 'image/gif');
+        $this->headers->set('Content-Length', 42);
+        $this->headers->set('Cache-Control', 'private, no-cache, no-cache=Set-Cookie, proxy-revalidate');
+        $this->headers->set('Pragma', 'no-cache');
     }
 }
--- src/Services/EmailService.php
+++ src/Services/EmailService.php
@@ -13,6 +13,7 @@ use Swift_Message;
 use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\String\ByteString;
 use Symfony\Component\Templating\EngineInterface;
 use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 use WebEtDesign\NewsletterBundle\Entity\Newsletter;
@@ -23,18 +24,20 @@ class EmailService
 {
 
     public function __construct(
-        private Swift_Mailer $mailer,
-        private EngineInterface $templating,
-        private ModelProvider $modelProvider,
-        private EntityManagerInterface $em,
+        private Swift_Mailer             $mailer,
+        private EngineInterface          $templating,
+        private ModelProvider            $modelProvider,
+        private EntityManagerInterface   $em,
         private EventDispatcherInterface $eventDispatcher,
-        private ?string $from,
-        private RouterInterface $router,
-        private array $locales,
-        private string $rootDir,
-        private bool $enableLog
+        private ?string                  $from,
+        private RouterInterface          $router,
+        private array                    $locales,
+        private string                   $rootDir,
+        private bool                     $enableLog
 
-    ) {}
+    )
+    {
+    }
 
     /**
      * @param Newsletter $newsletter
@@ -43,7 +46,7 @@ class EmailService
      * @return int
      * @throws Exception
      */
-    public function sendNewsletter(Newsletter $newsletter,  $email_list, FlashBagInterface $flashBag = null): int
+    public function sendNewsletter(Newsletter $newsletter, $email_list, FlashBagInterface $flashBag = null): int
     {
         $log = new Logger('mailer');
         $log->pushHandler(new StreamHandler($this->rootDir . '/var/log/mailer.log', Logger::DEBUG));
@@ -52,23 +55,38 @@ class EmailService
 
         foreach ($email_list as $locale => $emails) {
             foreach ($emails as $token => $email) {
-                try{
+                try {
                     $log->info('Mail to ' . $email . ' created');
-                    $link = $token ? $this->router->generate('newsletter_unsub', ['token' => $token], $this->router::ABSOLUTE_URL) : $this->router->generate('newsletter_unsub_auto', [], $this->router::ABSOLUTE_URL) ;
+                    $link = $token ? $this->router->generate('newsletter_unsub', ['token' => $token], $this->router::ABSOLUTE_URL) : $this->router->generate('newsletter_unsub_auto', [], $this->router::ABSOLUTE_URL);
+
+                    $html = $this->templating->render($this->modelProvider->getTemplate($newsletter->getModel()), [
+                        'object' => $newsletter,
+                        'locale' => $newsletter->isSendInAllLocales() ? $this->locales : [$locale],
+                        'unsub' => $link
+                    ]);
 
                     $trackingToken = md5(uniqid('', true));
-                    $tracker = $this->enableLog ? "<img src='" . $this->router->generate('newsletter_track_opening', ['token' => $trackingToken], UrlGeneratorInterface::ABSOLUTE_URL) . "'>" : '';
+
+                    if ($this->enableLog) {
+                        $tracker = "<img border=0 width=1 alt='' height=1  src='" . $this->router->generate('newsletter_track_opening', ['token' => $trackingToken], UrlGeneratorInterface::ABSOLUTE_URL) . "'>";
+
+                        $linebreak = ByteString::fromRandom(32)->toString();
+                        $html = str_replace("\n", $linebreak, $html);
+
+                        if (preg_match("/^(.*<body[^>]*>)(.*)$/", $html, $matches)) {
+                            $html = $matches[1] . $matches[2] . $tracker;
+                        } else {
+                            $html = $html . $tracker;
+                        }
+                        $html = str_replace($linebreak, "\n", $html);
+                    }
 
                     $message = (new Swift_Message($newsletter->getTitle()))
                         ->setFrom([$this->from ?: $newsletter->getEmail() => $newsletter->getSender()])
                         ->setTo($email)
                         ->setReplyTo($newsletter->getEmail())
                         ->setBody(
-                            $this->templating->render($this->modelProvider->getTemplate($newsletter->getModel()), [
-                                'object' => $newsletter,
-                                'locale' => $newsletter->isSendInAllLocales() ? $this->locales : [$locale],
-                                'unsub' => $link
-                            ]) . " " . $tracker, 'text/html'
+                            $html, 'text/html'
                         )
                         ->addPart(
                             $this->templating->render($this->modelProvider->getTxt($newsletter->getModel()), [
@@ -77,15 +95,19 @@ class EmailService
                                 'unsub' => $link
                             ]), 'text/plain'
                         );
+                    if ($this->enableLog){
+                        $message->getHeaders()->addTextHeader('X-Mailer-Hash', $trackingToken);
+                        $message->getHeaders()->has('X-No-Track') ? $message->getHeaders()->remove('X-No-Track') : null;
+                    }
 
                     $res = $this->mailer->send($message);
                     $this->eventDispatcher->dispatch(new MailSentEvent($message, $trackingToken, $newsletter->getId()), MailSentEvent::NAME);
-                }catch (Exception $e){
+                } catch (Exception $e) {
                     $log->error('Mail to ' . $email . ' error');
                     $flashBag?->add('error', "Le mail à l'adresse " . $email . " n'a pas été envoyé suite à une erreur. (" . $e->getMessage() . ')');
                     $res = -1;
                 }
-           }
+            }
         }
 
         return $res;
@@ -93,14 +115,14 @@ class EmailService
 
     public function getEmails(Newsletter $newsletter): array
     {
-        $unsubscribe = array_map(function(Unsubscribe $a){
+        $unsubscribe = array_map(function (Unsubscribe $a) {
             return $a->getEmail();
         }, $this->em->getRepository(Unsubscribe::class)->findAll());
 
         $emails = [];
 
 
-        if ($newsletter->getGroups()->count() > 0){
+        if ($newsletter->getGroups()->count() > 0) {
             $qb = $this->em->getRepository(User::class)->createQueryBuilder('u');
 
             $or = '(';
@@ -108,15 +130,15 @@ class EmailService
             foreach ($newsletter->getGroups() as $group) {
                 $or .= ':group_' . $cpt . ' MEMBER OF u.groups OR ';
                 $qb->setParameter('group_' . $cpt, $group);
-                $cpt ++;
+                $cpt++;
             }
             $or = substr($or, 0, strlen($or) - 3) . ')';
 
-            if (strlen($or) > 5){
+            if (strlen($or) > 5) {
                 $qb->andWhere($or);
             }
 
-            if (!empty($unsubscribe)){
+            if (!empty($unsubscribe)) {
                 $qb->andWhere(
                     $qb->expr()->notIn('u.email', ':unsub')
                 )
@@ -127,10 +149,10 @@ class EmailService
 
             /** @var User $u */
             foreach ($users as $u) {
-                if (!$u->getNewsletterToken()){
+                if (!$u->getNewsletterToken()) {
                     $u->setNewsletterToken(md5(uniqid()));
                 }
-                $locale = method_exists($u, 'getLocale') ? $u->getLocale() :  'fr';
+                $locale = method_exists($u, 'getLocale') ? $u->getLocale() : 'fr';
                 $locale = $locale !== '' && $locale !== null ? $locale : 'fr';
                 $emails[$locale][$u->getNewsletterToken()] = $u->getEmail();
             }
@@ -138,10 +160,10 @@ class EmailService
         }
 
         $more = $newsletter->getEmailsMoreArray();
-        if (!empty($more)){
+        if (!empty($more)) {
 
             foreach ($more['fr'] as $key => $item) {
-                if (in_array($item, $unsubscribe)){
+                if (in_array($item, $unsubscribe)) {
                     unset($more['fr'][$key]);
                 }
             }
